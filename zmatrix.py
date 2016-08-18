ea14d12153178e3f01806502d8c8aa4b1a4b1b7d
Dustin Tracy <dtracy@ufl.edu>
2016-08-18 11:22:31 -0400

    working version (testing for bugs)

--- zmatrix.py
+++ zmatrix.py
@@ -7,19 +7,41 @@ class ZMatrix(Coordinates):
         Coordinates.__init__(self, from_coordinate_string, charge, multiplicity, coordinates)
 
     def find_coordinates(self, coordinate_string):
-        p_atom = re.compile("(\s+\w{1,2}\s+(\d+\s+-?\d+\.\d+\s*)*)[\t ]*0*[\t ]*\n")
-        m = re.findall(p_atom, coordinate_string)
+        # Get coordinate block
+        p_block = re.compile("((.|\n)+\n\n)((.|\n)+)")
+        p = re.search(p_block, coordinate_string)
+        p_atom = re.compile("([ ]+\w{1,2}\s+(\d+\s+-?\d+\.\d+\s*)*)[\t ]*0*[\t ]*\n")
+        m = re.findall(p_atom, p.group(1))
         self.coordinates = []
         p_atom_type = re.compile("^\s*\w+")
-        p_coord = re.compile("\d+\s+-?\d+\.\d+\s*")
+        p_coord = re.compile("((\d+)\s+(-?\d+\.\d+)\s*)")
         for i, atom in enumerate(m):
             atom_t = re.search(p_atom_type, atom[0]).group(0)
+            self.coordinates.append(atom_t)
             values = re.findall(p_coord, atom[0])
-
-
+            for v in values:
+                self.coordinates.extend([int(v[1]), float(v[2])])
+        if p.group(3):
+            self.coordinates.append(p.group(3))
 
     def str(self):
         coord_str = str(self.charge) + " " + str(self.multiplicity) + "\n"
-        for i, value in enumerate(self.coordinates):
-            coord_str += str(value)
+        coords = self.coordinates
+        for i, value in enumerate(coords):
+            try:
+                if i == 0:
+                    coord_str += str(value) + "\n"
+                if i == 1:
+                    coord_str += coords[i] + 18*" " + str(coords[i+1]) + 4*" " + "{0:.8f}".format(coords[i+2]) + "\n"
+                if i == 4:
+                    coord_str += str(coords[i]) + 18*" " + str(coords[i+1]) + 4*" " + "{0:.8f}".format(coords[i+2])
+                    coord_str += 4*" " + str(coords[i+3]) + 2*" " + "{0:.8f}".format(coords[i+4]) + "\n"
+                if i >= 9 and ((i-9) % 7) == 0:
+                    coord_str += str(coords[i]) + 18*" " + str(coords[i+1]) + 4*" " + "{0:.8f}".format(coords[i+2])
+                    coord_str += 4*" " + str(coords[i+3]) + 2*" " + "{0:.8f}".format(coords[i+4])
+                    coord_str += 4*" " + str(coords[i+5]) + 2*" " + "{0:.8f}".format(coords[i+6])
+                    coord_str += str(2*"\t" + str(0) + "\n")
+            except IndexError:
+                coord_str += "\n" + coords[-1]
+
         return coord_str
