9d12705d7fe40a5c9785781ffb2bea07af73a572
Dustin Tracy <dtracy@ufl.edu>
2016-08-19 13:04:25 -0400

    switched to pickle!

--- reparm_data.py
+++ reparm_data.py
@@ -1,5 +1,6 @@
 import re
 import copy
+import pickle
 from reparm_input import ReparmInput
 from parameters import Parameters
 from coordinates import Coordinates
@@ -25,110 +26,11 @@ class ReparmData():
         return self
 
     def save(self):
-        number_geometries = self.reparm_input.number_geometries
-        temperature = self.reparm_input.temperature
-        number_atoms = len(self.best_am1_individual.inputs[0].coordinates[0].coordinates)
-        charge = self.best_am1_individual.inputs[0].coordinates[0].charge
-        multiplicity = self.best_am1_individual.inputs[0].coordinates[0].multiplicity
-        inputs = self.best_am1_individual.inputs
-        parameters = self.best_am1_individual.inputs[0].parameters[0]
-
-
-        coordinates = []
-        for i in inputs:
-            for j in i.coordinates[0].coordinates:
-                for k in j:
-                    coordinates.append(k)
-
-        energies = []
-        for i in self.high_level_outputs:
-            energies.append(i.ground_energy)
-
-        dipoles = []
-        for i in self.high_level_outputs:
-            for j in i.dipole:
-                dipoles.append(j)
-
-        fout = open("reparm.dat", 'w')
-        fout.write(str(self.original_fitness) + "\n")
-        fout.write(str(number_geometries) +"\n")
-        fout.write(str(temperature) + "\n")
-        fout.write(str(number_atoms) +"\n")
-        fout.write(str(charge) +"\n")
-        fout.write(str(multiplicity) +"\n")
-        fout.write(str(len(coordinates)) +"\n")
-        for i in coordinates:
-            fout.write(str(i) +"\n")
-        fout.write(str(len(energies)) +"\n")
-        for i in energies:
-            fout.write(str(i) +"\n")
-        fout.write(str(len(dipoles)) +"\n")
-        for i in dipoles:
-            fout.write(str(i) +"\n")
-        fout.write(parameters.str())
+        pickle.dump(self, open("reparm.dat", "wb"))
 
     def load(self):
-        fin = open("reparm.dat", 'r')
-        of = fin.readline()
-        if of != "None\n":
-            self.original_fitness = float(of)
-        number_geometries = int(fin.readline())
-        temperature = float(fin.readline())
-        number_atoms = int(fin.readline())
-        charge = int(fin.readline())
-        multiplicity = int(fin.readline())
-
-        # Read the cooordinates
-        vector_size = int(fin.readline())
-        coordinates = []
-        for _ in range(number_geometries):
-            coord_data = []
-            for _ in range(0, int(vector_size/number_geometries), 4):
-                coordinate = [int(fin.readline()),
-                              float(fin.readline()),
-                              float(fin.readline()),
-                              float(fin.readline())]
-                coord_data.append(coordinate)
-            coordinates.append(Coordinates(charge=charge, multiplicity=multiplicity,
-                                           coordinates=coord_data))
-
-        # Read the energies
-        vector_size = int(fin.readline())
-        energies = []
-        for _ in range(vector_size):
-            energies.append(float(fin.readline()))
-
-        # Read the dipoles
-        vector_size = int(fin.readline())
-        dipoles = []
-        for _ in range(vector_size):
-            dipoles.append(float(fin.readline()))
-
-        # Read the parameters
-        param_s = fin.read()
-        fin.close()
-        params = Parameters(from_parameter_string=param_s)
-
-        # Have now loaded all the info for the best am1 individual
-        header_string = "#P AM1(Input,Print)\n\nAM1\n"
-        header = Header(header_string)
-        inputs = []
-        for i in range(number_geometries):
-            gin = GaussianInput(header=header, coordinates=coordinates[i],
-                                parameters=params)
-            inputs.append(gin)
-        self.best_am1_individual = ParameterGroup(inputs=inputs)
-
-        # Make the HighLevelTheory
-        hlts = []
-        for i in range(number_geometries):
-            gout = GaussianOutput()
-            gout.ground_energy = energies[i]
-            v_dipole = []
-            for j in range(3):
-                v_dipole.append(dipoles[3*i + j])
-            gout.dipole = v_dipole
-            hlts.append(gout)
-        self.high_level_outputs = hlts
-
+        lf = pickle.load(open("reparm.dat", 'rb'))
+        self.original_fitness = lf.original_fitness
+        self.high_level_outputs = lf.high_level_outputs
+        self.best_am1_individual = lf.best_am1_individual
 
