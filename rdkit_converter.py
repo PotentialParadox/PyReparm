ac5d8959f6a39828a47f82d3e5cbba9be47149c0
Dustin Tracy <dtracy@ufl.edu>
2016-08-28 11:06:31 -0400

    fixe np bug

--- rdkit_converter.py
+++ rdkit_converter.py
@@ -1,22 +1,20 @@
 import numpy as np
+import rdkit
 
-arra = np.array([1, 2])
+from gaussian_input import GaussianInput
+import openbabel
 
-# from gaussian_input import GaussianInput
-# import openbabel
-#
-# obConversion = openbabel.OBConversion()
-# obConversion.SetInAndOutFormats("xyz", "mol")
-#
-# gin = GaussianInput(open("test.com", 'r').read())
-# coordinates = gin.coordinates[0]
-# xyz_str = coordinates.xyz_string()
-# print(xyz_str)
-#
-# xyz = openbabel.OBMol()
-# obConversion.ReadString(xyz, xyz_str)
-#
-# mol = obConversion.WriteString(xyz)
-# print(mol)
-#
+obConversion = openbabel.OBConversion()
+obConversion.SetInAndOutFormats("xyz", "mol")
+
+gin = GaussianInput(open("test.com", 'r').read())
+coordinates = gin.coordinates[0]
+xyz_str = coordinates.xyz_string()
+print(xyz_str)
+
+xyz = openbabel.OBMol()
+obConversion.ReadString(xyz, xyz_str)
+
+mol = obConversion.WriteString(xyz)
+
 
